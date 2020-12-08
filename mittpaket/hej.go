f4f4cbc1865d22e32fac1b75700693d43b40f5e5
pomBergstrom <pom.bergstrom@gmail.com>
2020-12-08 20:25:22 +0100

    2:nd

--- mittpaket/hej.go
+++ mittpaket/hej.go
@@ -2,6 +2,7 @@ package mittpaket
 
 import "rsc.io/quote"
 
+// SayHej : hälsa på alla
 func SayHej() string {
 	//return "Hej hopp"
 	//println("hej")
--- mittpaket/hej_test.go
+++ mittpaket/hej_test.go
@@ -1,13 +1,18 @@
 package mittpaket
 
-import "testing"
+import (
+	"circletest/mittpaket"
+	"testing"
+)
 
 func TestHej(t *testing.T) {
 
+	var s = mittpaket.SayHej()
 	expected := "This will fail"
-	if ret := SayHej(); ret != expected {
+	if ret := s; ret != expected {
 
-		t.Errorf("Hej() = %q, want %q", ret, expected)
+		//t.Errorf("Hej() = %q, want %q", ret, expected)
+		//t.Logf("Hej() = %q, want %q", ret, expected)
 
 	}
 
